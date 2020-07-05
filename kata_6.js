2a9ff1b1ae01db0d0685ffe47baf72cd2a0636f5
Kify <kflores.valona@gmail.com>
2020-07-05 14:01:09 -0500

    add kata_6

---
+++ kata_6.js
@@ -0,0 +1,21 @@
+/* Crea una función llamada longestCountry(), esta función recibirá de parámetro un arreglo con nombres de distintos países, 
+la función deberá retornar el país con mayor número de letras.
+Por ejemplo, del siguiente arreglo de países:
+const paises = ["México", "Panamá", "Guatemala", "El Salvador"]
+El Salvador es el país con mayor número de letras. */
+
+
+function longestCountry(arreglo) {
+    var lgth = 0;
+    var longest;
+
+    for(var i=0; i < arreglo.length; i++){
+        if(arreglo[i].length > lgth){
+        var lgth = arreglo[i].length;
+        longest = arreglo[i];
+        }     
+    } 
+    return longest;
+}
+ 
+console.log(longestCountry(["México", "Panamá", "Guatemala", "El Salvador"]));
